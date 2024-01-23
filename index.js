eee2649913dc7b0d1fed14d01a31e94ab8d87f2a
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2024-01-23 21:34:06 +0500

    Min

---
+++ index.js
@@ -0,0 +1,157 @@
+
+// let number = 243;
+// let xonalar = 0;
+// while (number >= 1) {
+//     number = Math.trunc(number / 10)
+//     xonalar++
+// }
+// console.log(xonalar);
+
+
+//  HOMEWORK
+
+/**
+ * 1 masala
+ */
+// let oy = +prompt(` oyning kiriting`)
+// let res = ` `
+// switch (oy) {
+//     case 1:
+//         res = `Yanvar`
+//         break;
+//     case 2:
+//         res = `Fevral`
+//         break;
+
+//     case 3:
+//         res = `Mart`
+//         break;
+
+//     case 4:
+//         res = `Aprel`
+//         break;
+
+//     case 5:
+//         res = `May`
+//         break;
+
+//     case 6:
+//         res = `Iyun`
+//         break;
+
+//     case 7:
+//         res = `Iyul`
+//         break;
+
+//     case 8:
+//         res = `Avgust`
+//         break;
+
+//     case 9:
+//         res = `Sentyabr`
+//         break;
+
+//     case 10:
+//         res = `Oktyabr`
+//         break;
+
+//     case 11:
+//         res = `Noyabr`
+//         break;
+
+//     case 12:
+//         res = `Dekabr`
+//         break;
+
+//     default:
+//         res = `Bunday oy mavjud emas`
+// }
+// console.log(res);
+
+/**
+ * 2 Masala
+ */
+// let oy = 12;
+// let res = ` `
+// switch (oy) {
+//     case 1:
+//         res = `Qish`
+//         break;
+//     case 2:
+//         res = `Qish`
+//         break;
+
+//     case 3:
+//         res = `Bahor`
+//         break;
+
+//     case 4:
+//         res = `Bahor`
+//         break;
+
+//     case 5:
+//         res = `Bahor`
+//         break;
+
+//     case 6:
+//         res = `Yoz`
+//         break;
+
+//     case 7:
+//         res = `Yoz`
+//         break;
+
+//     case 8:
+//         res = `Yoz`
+//         break;
+
+//     case 9:
+//         res = `Kuz`
+//         break;
+
+//     case 10:
+//         res = `Kuz`
+//         break;
+
+//     case 11:
+//         res = `Kuz`
+//         break;
+
+//     case 12:
+//         res = `Qish`
+//         break;
+
+//     default:
+//         res = `Bunday fasl mavjud emas`
+// }
+// console.log(res);
+
+/**
+ * 3 Masala
+ */
+
+/**
+ * 4 Masala
+ */
+
+/**
+ * 5 Masala
+ * 1 dan n gacha bolgan sonlarni teskari tartbd chiqaruv
+ */
+// for(let n = 12; n >= 1; n--){
+//     console.log(n);
+// }
+
+
+/**
+ * 6 Masala
+ */
+let son = 5; 
+let res;
+if (son == 0 || son == 1) {
+    res = 1;
+} else {
+    res = son * son - 1;
+  }
+console.log(res);
+
